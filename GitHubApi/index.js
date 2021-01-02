8d9dab02eedcc268da5a4f0de7a51117a7d201ee
cormacmadden <comadden@Gmail.com>
2021-01-02 19:34:36 +0000

    New Basic API fetches

--- GitHubApi/index.js
+++ GitHubApi/index.js
@@ -1,76 +1,24 @@
 //javascript
 const apiData = {
-    url: '',
-    type: 'pokemon',
-    id: '25',
+    url: 'https://api.github.com/',
+    type: 'users',
+    id: 'defunkt',
 }
 
+const apiURL = `${apiData.url}${apiData.type}/${apiData.id}`
+
+fetch(apiURL) 
+    .then(data => data.json())
+    .then((user) => generateHTML(user))
+
+const generateHTML = (data) => {
+    console.log(data)
+    const html = `
+        <div class="name"> ${data.name}<\div>
+        <div class="bio"> ${data.bio}<\div>
+        `
+    const d3Div = document.querySelector('.GitHub_d3')
+    d3Div.innerHTML = html
+}
 
 d3.select();
-d3.selectAll();
-
-d3.select('h1').style('color', 'red')
-.attr('class', 'heading')
-.text('Updated h1 tag')
-
-d3.select('body');
-
-fetch('https://reqres.in/api/users', {
-    method: 'POST',
-    headers:{
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        name: 'User 1'
-    })
-})
-.then(res => {
-    return res.json()
-})
-.then(data => console.log(data))
-.catch(error => console.log('ERROR'))
-
-  
-
-/*
-chart = {
-    const root = pack(data);
-    
-    const svg = d3.create("svg")
-        .attr("viewBox", [0, 0, width, height])
-        .attr("font-size", 10)
-        .attr("font-family", "sans-serif")
-        .attr("text-anchor", "middle");
-  
-    const leaf = svg.selectAll("g")
-      .data(root.leaves())
-      .join("g")
-        .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
-  
-    leaf.append("circle")
-        .attr("id", d => (d.leafUid = DOM.uid("leaf")).id)
-        .attr("r", d => d.r)
-        .attr("fill-opacity", 0.7)
-        .attr("fill", d => color(d.data.group));
-  
-    leaf.append("clipPath")
-        .attr("id", d => (d.clipUid = DOM.uid("clip")).id)
-      .append("use")
-        .attr("xlink:href", d => d.leafUid.href);
-  
-    leaf.append("text")
-        .attr("clip-path", d => d.clipUid)
-      .selectAll("tspan")
-      .data(d => d.data.name.split(/(?=[A-Z][a-z])|\s+/g))
-      .join("tspan")
-        .attr("x", 0)
-        .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
-        .text(d => d);
-  
-    leaf.append("title")
-        .text(d => `${d.data.title === undefined ? "" : `${d.data.title}
-  `}${format(d.value)}`);
-      
-    return svg.node();
-  }
-  */
